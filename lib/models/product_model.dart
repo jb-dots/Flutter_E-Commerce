a20e49436d64dca389253f36a28dc0087e8a501b
jb-dots <j.dutaro.545524@umindanao.edu.ph>
2025-11-15 01:06:56 +0800

    Bili na kayow

---
+++ lib/models/product_model.dart
@@ -0,0 +1,46 @@
+class Product{
+  final String name;
+  final String price;
+  final String status;
+  final String image;
+  final String category;
+
+  Product({
+    required this.name,
+    required this.price,
+    required this.status,
+    required this.image,
+    required this.category,
+  });
+}
+
+final List<Product> products = [
+  Product(
+    name: 'Nike Air Max 200',
+    price: '180.0',
+    status: 'Trending Now',
+    image: 'assets/shoes1.jpg',
+    category: 'Sneakers',
+  ),
+  Product(
+    name: 'Nike Originals',
+    price: '120.0',
+    status: 'Out of Stock',
+    image: 'assets/shoes2.jpg',
+    category: 'Sneakers',
+  ),
+  Product(
+    name: 'Adidas Hoodie Jacket',
+    price: '90.0',
+    status: 'Trending Now',
+    image: 'assets/jacket1.jpg',
+    category: 'Jackets',
+  ),
+  Product(
+    name: 'Leather Jacket',
+    price: '130.0',
+    status: 'Out of Stock',
+    image: 'assets/jacket2.jpg',
+    category: 'Jackets',
+  ),
+];
